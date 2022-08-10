35873da1df4aad66fef6cf4044e763375af9a81d
RichDoherty <doherty622@gmail.com>
2022-08-10 10:17:20 -0400

    Notes App
    Added my new note taking app to the projects section.

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Vulf from './img/vulf-site.png';
 import JpFlashcards from './img/jp-flashcards.png';
-import Calculator from './Calculator';
+import NotesApp from './img/notes-app.png'
 import './Projects.css';
 import './style.css';
 
@@ -16,11 +16,39 @@ class Projects extends Component {
           <div className="project-card">
 
             <div className="project-name">
+              <h1>Note Taking App</h1>
+              <div className="img-shadow">
+                <div className="img-border">
+                  <a href="https://stackblitz.com/edit/angular-ivy-avykfm?file=src/app/note-create/note-create.component.ts" target="_blank" rel="noreferrer">
+                    <img className="project-img" src={NotesApp} alt="Notes App" />
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="project-description">
+              <p>
+                This is a simple note taking app. The notes are stored <br /> in the browser using localStorage. <br /> <br /> Created with Angular, TypeScript, HTML, and CSS.
+              </p>
+            </div>
+              <div className="links">
+                <a href="https://stackblitz.com/edit/angular-ivy-avykfm?file=src/app/note-create/note-create.component.ts" target="_blank" rel="noreferrer">
+                  View Project
+                </a>
+                <a href="https://github.com/RichDoherty/Notes-App" target="_blank" rel="noreferrer">
+                  View Code
+                </a>
+              </div>
+            </div>
+
+          <div className="project-card">
+
+            <div className="project-name">
               <h1>Japanese Flashcards</h1>
               <div className="img-shadow">
                 <div className="img-border">
-                  <a href="https://richdoherty.github.io/japanese-flashcards/" target="_blank">
-                    <img className="project-img" src={JpFlashcards} alt="Project 2." />
+                  <a href="https://richdoherty.github.io/japanese-flashcards/" target="_blank" rel="noreferrer">
+                    <img className="project-img" src={JpFlashcards} alt="Japanese Flashcards" />
                   </a>
                 </div>
               </div>
@@ -32,10 +60,10 @@ class Projects extends Component {
               </p>
             </div>
               <div className="links">
-                <a href="https://richdoherty.github.io/japanese-flashcards/" target="_blank">
+                <a href="https://richdoherty.github.io/japanese-flashcards/" target="_blank" rel="noreferrer">
                   View Project
                 </a>
-                <a href="https://github.com/RichDoherty/japanese-flashcards" target="_blank">
+                <a href="https://github.com/RichDoherty/japanese-flashcards" target="_blank" rel="noreferrer">
                   View Code
                 </a>
               </div>
@@ -47,7 +75,7 @@ class Projects extends Component {
               <h1>Vulpeck Fan-Site</h1>
               <div className="img-shadow">
                 <div className="img-border">
-                  <a href="https://richdoherty.github.io/Vulfpeck/index.html" target="_blank">
+                  <a href="https://richdoherty.github.io/Vulfpeck/index.html" target="_blank" rel="noreferrer">
                     <img className="project-img" src={Vulf} alt="A screenshot of my Vulpeck fan site." />
                   </a>
                 </div>
@@ -60,10 +88,10 @@ class Projects extends Component {
               </p>
             </div>
               <div className="links">
-                <a href="https://richdoherty.github.io/Vulfpeck/index.html" target="_blank">
+                <a href="https://richdoherty.github.io/Vulfpeck/index.html" target="_blank" rel="noreferrer">
                   View Project
                 </a>
-                <a href="https://github.com/RichDoherty/Vulfpeck" target="_blank">
+                <a href="https://github.com/RichDoherty/Vulfpeck" target="_blank" rel="noreferrer">
                   View Code
                 </a>
               </div>
